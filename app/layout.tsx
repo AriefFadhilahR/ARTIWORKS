86334d8653cb6a1d498478ec312a5987e706148c
Ardhian <4scotchrip@gmail.com>
2026-05-18 21:23:55 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="id">
-      <body>{children}</body>
+      <body style={{ margin: 0, padding: 0 }}>{children}</body>
     </html>
   )
 }
