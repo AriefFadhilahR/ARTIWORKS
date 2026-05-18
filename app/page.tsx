71a9c2764c1f98236d94f3636bc15aad01c1184a
Ardhian <4scotchrip@gmail.com>
2026-05-18 14:36:51 +0700

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '2rem', textAlign: 'center' }}>
+      <h1>Artiworks</h1>
+      <p>Halo, saya Arief. Website ini sedang dibangun.</p>
+    </div>
+  )
+}
